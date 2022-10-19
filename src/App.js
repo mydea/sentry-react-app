5a572804fcd4c2f97a6b4767d25883b6e87442c7
Francesco Novy <francesco.novy@sentry.io>
2022-10-19 13:43:07 +0200

    Install sentry
    Make sure to set `REACT_APP_SENTRY_DSN` env variable

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+function throwError() {
+  throw new Error("what is happening here?");
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to this amazing test app!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +22,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={throwError}>Break the world</button>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
